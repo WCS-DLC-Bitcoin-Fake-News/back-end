1b5d50ad8b41c48e7144cf9c56af11953c93bdbe
Okan Karakaya <okankarakaya@windowslive.com>
2021-06-08 18:57:53 +0200

    create the skeleton

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import users from "./routes/users.js";
+import connectDB from "./db.js";
+const app = express();
+
+//connect to database
+connectDB();
+//parse json
+app.use(express.json());
+//
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/users", users);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
