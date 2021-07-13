90d8375003b1583380234acfe606aadba844d987
Carlos Yarza <carlosyarza@gmail.com>
2021-07-13 23:44:01 +0200

    create Middleware scrap

---
+++ middleware/scrap.js
@@ -0,0 +1,35 @@
+import puppeteer from 'puppeteer';
+
+
+
+const scrap = async (req, res, next) => {
+    console.log("i am in my middleware")
+    console.log(req.body.source)
+    try {
+        console.log("hello")
+        const browser = await puppeteer.launch();   
+        const page = await browser.newPage();
+        console.log("hello2")
+
+        //await page.waitForSelector('is-loaded')
+        await page.waitForFunction('document.getElementsByClassName("is-loaded").length');
+        await page.goto(req.body.source);
+
+
+        console.log("hello5")
+        const fileName = Math.random().toString(36).substring(7) + ".pdf";
+        
+        console.log(fileName);
+        await page.pdf({ path: `public/${fileName}`, format: 'a4' });
+        console.log("good bye")
+
+        await browser.close();  
+        req.body.printedSource = `${fileName}`; 
+        return next();
+    } catch (error) {
+        res.json(error);
+    }
+
+}
+
+export default scrap;
