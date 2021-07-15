2eb29be513995b4b22adeabd9f69a9702d46de5b
Carlos Yarza <carlosyarza@gmail.com>
2021-07-15 11:44:00 +0200

    refactor scrap middleware & working

--- middleware/scrap.js
+++ middleware/scrap.js
@@ -1,30 +1,16 @@
 import puppeteer from 'puppeteer';
 
-
-
 const scrap = async (req, res, next) => {
-    console.log("i am in my middleware")
     console.log(req.body.source)
     try {
-        console.log("hello")
-        const browser = await puppeteer.launch();   
-        console.log("hello2")
-
-        await page.waitForSelector('#is-loaded')
-
-        await page.goto(source{});
-
-
-
-
-
-        console.log("hello5")
+        const browser = await puppeteer.launch();  
+        const page = await browser.newPage();
+        await page.goto(req.body.source, {
+            waitUntil: 'networkidle2',
+        });
         const fileName = Math.random().toString(36).substring(7) + ".pdf";
-        
         console.log(fileName);
         await page.pdf({ path: `public/${fileName}`, format: 'a4' });
-        console.log("good bye")
-
         await browser.close();  
         req.body.printedSource = `${fileName}`; 
         return next();
